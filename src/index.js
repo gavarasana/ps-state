f70cee836994fb6f10606ba408161e96bf97a804
gavarasana <gavarasana@gmail.com>
2021-02-14 13:51:45 -0500

    created custom hook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ErrorBoundary from "./common/ErrorBoundary";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
