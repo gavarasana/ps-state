1ff8fd5d601ba04d42b92bddbe133b06a912b948
gavarasana <gavarasana@gmail.com>
2021-02-15 15:48:20 -0500

    added detail and cart pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import {  Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import Home from "./Home";
 import Products from "./Products";
 import Detail from "./Detail";
 import Cart from "./Cart";
 
 export default function App() {
-
-
   return (
     <>
       <div className="content">
         <Header />
         <main>
-            <Switch>
-              <Route exact path="/" component={Products}/>
-              <Route path="/Detail" component={Detail} />
-              <Route path="/Cart" component={Cart} />
-            </Switch>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Cart" component={Cart} />
+            <Route exact path="/:category" component={Products} />
+            <Route path="/:category/:id" component={Detail} />
+          </Switch>
         </main>
       </div>
       <Footer />
--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useParams, useHistory } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
 
 export default function Detail() {
-    return (
-        <h1>Detail</h1>
-    );
+  const { id: productId } = useParams();
+  const history = useHistory();
+
+  const { data: product, error, loading } = useFetch(`products/${productId}`);
+
+  if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <p>
+        <button
+          className="btn btn-primary"
+          onClick={() => history.push("/cart")}
+        >
+          Add to card
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
